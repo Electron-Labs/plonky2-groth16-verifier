4a0290b2efbf9f0d46fa44e4a8319a38a03f9e35
electron-team <utsavjn48@gmail.com>
2023-10-25 18:24:10 +0530

    chore: plonky2 config not required
    Signed-off-by: electron-team <utsavjn48@gmail.com>

--- cmd/build.go
+++ cmd/build.go
@@ -23,7 +23,7 @@ var buildCmd = &cobra.Command{
 	Short: "Build gnark groth16 circuit",
 	Long:  `Builds gnark groth16 circuit corresponding to provided common_data and plonky2 config.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("build called with config at\n %s,\n common data at\n %s ", config_path, common_data_path)
+		fmt.Printf("build called with common data at %s\n ", common_data_path)
 		var myCircuit verifier.Verifier
 		r1cs, _ := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &myCircuit)
 		pk, vk, _ := groth16.Setup(r1cs)
@@ -41,8 +41,6 @@ var buildCmd = &cobra.Command{
 }
 
 func init() {
-	buildCmd.Flags().StringVarP(&config_path, "config", "c", "", "JSON File path to plonky2 config")
-	_ = buildCmd.MarkFlagRequired("config")
 	buildCmd.Flags().StringVarP(&common_data_path, "common_data", "d", "", "JSON File path to common data of plonky2 circuit")
 	_ = buildCmd.MarkFlagRequired("common_data")
 	rootCmd.AddCommand(buildCmd)
