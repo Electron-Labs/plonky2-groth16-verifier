77211675eb9686a9dec9f84911f7c0dd176bb708
ultrainstinct30 <parth@atomlabs.one>
2023-11-06 01:01:08 +0530

    feat: basic poseidon

---
+++ goldilocks/poseidon.go
@@ -0,0 +1,83 @@
+package goldilocks
+
+import (
+	"math/big"
+
+	"github.com/consensys/gnark/frontend"
+)
+
+func Constant(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable, c []*big.Int, r int) []GoldilocksVariable {
+	out := make([]GoldilocksVariable, SPONGE_WIDTH)
+	for i, v := range in {
+		out[i] = Add(api, rangeChecker, v, GoldilocksVariable{Limb: c[i+r]})
+	}
+	return out
+}
+
+func Sbox(api frontend.API, rangeChecker frontend.Rangechecker, in GoldilocksVariable) GoldilocksVariable {
+	in2 := Mul(api, rangeChecker, in, in)
+	in4 := Mul(api, rangeChecker, in2, in2)
+	in3 := Mul(api, rangeChecker, in, in2)
+	return Mul(api, rangeChecker, in3, in4)
+}
+
+func Mds(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable, mds_circ []*big.Int, mds_diag []*big.Int) []GoldilocksVariable {
+	out := make([]GoldilocksVariable, SPONGE_WIDTH)
+	for i := 0; i < SPONGE_WIDTH; i++ {
+		lc := GoldilocksVariable{Limb: 0}
+		for j := 0; j < SPONGE_WIDTH; j++ {
+			lc = Add(api, rangeChecker, lc, Mul(api, rangeChecker, GoldilocksVariable{Limb: mds_circ[j]}, in[(i+j)%SPONGE_WIDTH]))
+		}
+		out[i] = Add(api, rangeChecker, lc, Mul(api, rangeChecker, in[i], GoldilocksVariable{Limb: mds_diag[i]}))
+	}
+	return out
+}
+
+func Permute(api frontend.API, rangeChecker frontend.Rangechecker, inputs []GoldilocksVariable) []GoldilocksVariable {
+	full_rounds_half := 4
+	partial_rounds := 22
+	constants := POSEIDON_CONSTANTS()
+	if len(constants) != MAX_WIDTH*N_ROUNDS {
+		panic("Incorrect number of constants")
+	}
+	mds_circ := POSEIDON_MDS_CIRC()
+	if len(mds_circ) != SPONGE_WIDTH {
+		panic("Incorrect number of constants")
+	}
+	mds_diag := POSEIDON_MDS_DIAG()
+	if len(mds_diag) != SPONGE_WIDTH {
+		panic("Incorrect number of constants")
+	}
+
+	state := make([]GoldilocksVariable, SPONGE_WIDTH)
+	for j := 0; j < SPONGE_WIDTH; j++ {
+		state[j] = inputs[j]
+	}
+
+	r := 0
+	for i := 0; i < full_rounds_half; i++ {
+		state = Constant(api, rangeChecker, state, constants, r*SPONGE_WIDTH)
+		for j := 0; j < SPONGE_WIDTH; j++ {
+			state[j] = Sbox(api, rangeChecker, state[j])
+		}
+		state = Mds(api, rangeChecker, state, mds_circ, mds_diag)
+		r += 1
+	}
+
+	for i := 0; i < partial_rounds; i++ {
+		state = Constant(api, rangeChecker, state, constants, r*SPONGE_WIDTH)
+		state[0] = Sbox(api, rangeChecker, state[0])
+		state = Mds(api, rangeChecker, state, mds_circ, mds_diag)
+		r += 1
+	}
+
+	for i := 0; i < full_rounds_half; i++ {
+		state = Constant(api, rangeChecker, state, constants, r*SPONGE_WIDTH)
+		for j := 0; j < SPONGE_WIDTH; j++ {
+			state[j] = Sbox(api, rangeChecker, state[j])
+		}
+		state = Mds(api, rangeChecker, state, mds_circ, mds_diag)
+		r += 1
+	}
+	return state
+}
---
+++ goldilocks/poseidon_test.go
@@ -0,0 +1,75 @@
+package goldilocks
+
+import (
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/std/rangecheck"
+	"github.com/consensys/gnark/test"
+)
+
+type TestPermuteCircuit struct {
+	Inputs  []GoldilocksVariable
+	Outputs []GoldilocksVariable
+}
+
+func (circuit *TestPermuteCircuit) Define(api frontend.API) error {
+	rangeChecker := rangecheck.New(api)
+	outputs := Permute(api, rangeChecker, circuit.Inputs)
+	for i := 0; i < SPONGE_WIDTH; i++ {
+		api.AssertIsEqual(outputs[i].Limb, circuit.Outputs[i].Limb)
+	}
+	return nil
+}
+
+func TestPermute(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	type testData struct {
+		inputs  []uint64
+		outputs []uint64
+	}
+
+	tests := []testData{
+		{inputs: []uint64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, outputs: []uint64{
+			0x3c18a9786cb0b359, 0xc4055e3364a246c3, 0x7953db0ab48808f4, 0xc71603f33a1144ca,
+			0xd7709673896996dc, 0x46a84e87642f44ed, 0xd032648251ee0b3c, 0x1c687363b207df62,
+			0xdf8565563e8045fe, 0x40f5b37ff4254dae, 0xd070f637b431067c, 0x1792b1c4342109d7,
+		}},
+		{inputs: []uint64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, outputs: []uint64{
+			0xd64e1e3efc5b8e9e, 0x53666633020aaa47, 0xd40285597c6a8825, 0x613a4f81e81231d2,
+			0x414754bfebd051f0, 0xcb1f8980294a023f, 0x6eb2a9e4d54a9d0f, 0x1902bc3af467e056,
+			0xf045d5eafdc6021f, 0xe4150f77caaa3be5, 0xc9bfd01d39b50cce, 0x5c0a27fcb0e1459b,
+		}},
+	}
+
+	var circuit TestPermuteCircuit
+	circuit.Inputs = make([]GoldilocksVariable, SPONGE_WIDTH)
+	circuit.Outputs = make([]GoldilocksVariable, SPONGE_WIDTH)
+	r1cs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
+	if err != nil {
+		t.Fatal("Error in compiling circuit: ", err)
+	}
+
+	for _, t_i := range tests {
+		var witness TestPermuteCircuit
+		witness.Inputs = make([]GoldilocksVariable, SPONGE_WIDTH)
+		witness.Outputs = make([]GoldilocksVariable, SPONGE_WIDTH)
+		for i := 0; i < SPONGE_WIDTH; i++ {
+			witness.Inputs[i] = GetGoldilocksVariable(t_i.inputs[i])
+			witness.Outputs[i] = GetGoldilocksVariable(t_i.outputs[i])
+		}
+
+		w, err := frontend.NewWitness(&witness, ecc.BN254.ScalarField())
+		if err != nil {
+			t.Fatal("Error in witness: ", err, "\n test: ", t_i)
+		}
+		err = r1cs.IsSolved(w)
+		if err != nil {
+			t.Fatal("Circuit not solved: ", err, "\n test: ", t_i)
+		}
+		assert.CheckCircuit(&circuit, test.WithValidAssignment(&witness), test.WithCurves(ecc.BN254))
+	}
+}
