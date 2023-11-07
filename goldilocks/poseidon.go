ca212e23544383da6d9bed3ec766b613661c4a39
ultrainstinct30 <parth@atomlabs.one>
2023-11-07 18:26:24 +0530

    chore: restructuring permute function

--- goldilocks/poseidon.go
+++ goldilocks/poseidon.go
@@ -6,12 +6,18 @@ import (
 	"github.com/consensys/gnark/frontend"
 )
 
-func Constant(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable, c []*big.Int, r int) []GoldilocksVariable {
-	out := make([]GoldilocksVariable, SPONGE_WIDTH)
+var CONSTANTS []*big.Int = POSEIDON_CONSTANTS()
+var MDS_CIRC []*big.Int = POSEIDON_MDS_CIRC()
+var MDS_DIAG []*big.Int = POSEIDON_MDS_DIAG()
+
+const FULL_ROUNDS_HALF = 4
+const PARTIAL_ROUNDS = 22
+
+func Constant(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable, r int) []GoldilocksVariable {
 	for i, v := range in {
-		out[i] = Add(api, rangeChecker, v, GoldilocksVariable{Limb: c[i+r]})
+		in[i] = Add(api, rangeChecker, v, GoldilocksVariable{Limb: CONSTANTS[i+r]})
 	}
-	return out
+	return in
 }
 
 func Sbox(api frontend.API, rangeChecker frontend.Rangechecker, in GoldilocksVariable) GoldilocksVariable {
@@ -21,35 +27,43 @@ func Sbox(api frontend.API, rangeChecker frontend.Rangechecker, in GoldilocksVar
 	return Mul(api, rangeChecker, in3, in4)
 }
 
-func Mds(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable, mds_circ []*big.Int, mds_diag []*big.Int) []GoldilocksVariable {
+func Mds(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable) []GoldilocksVariable {
 	out := make([]GoldilocksVariable, SPONGE_WIDTH)
 	for i := 0; i < SPONGE_WIDTH; i++ {
 		lc := GoldilocksVariable{Limb: 0}
 		for j := 0; j < SPONGE_WIDTH; j++ {
-			lc = Add(api, rangeChecker, lc, Mul(api, rangeChecker, GoldilocksVariable{Limb: mds_circ[j]}, in[(i+j)%SPONGE_WIDTH]))
+			lc = Add(api, rangeChecker, lc, Mul(api, rangeChecker, GoldilocksVariable{Limb: MDS_CIRC[j]}, in[(i+j)%SPONGE_WIDTH]))
 		}
-		out[i] = Add(api, rangeChecker, lc, Mul(api, rangeChecker, in[i], GoldilocksVariable{Limb: mds_diag[i]}))
+		out[i] = Add(api, rangeChecker, lc, Mul(api, rangeChecker, in[i], GoldilocksVariable{Limb: MDS_DIAG[i]}))
 	}
 	return out
+}
+
+func FullRounds(api frontend.API, rangeChecker frontend.Rangechecker, state []GoldilocksVariable, r *int) []GoldilocksVariable {
+	for i := 0; i < FULL_ROUNDS_HALF; i++ {
+		state = Constant(api, rangeChecker, state, *r*SPONGE_WIDTH)
+		for j := 0; j < SPONGE_WIDTH; j++ {
+			state[j] = Sbox(api, rangeChecker, state[j])
+		}
+		state = Mds(api, rangeChecker, state)
+		*r += 1
+	}
+	return state
+}
+
+func PartialRounds(api frontend.API, rangeChecker frontend.Rangechecker, state []GoldilocksVariable, r *int) []GoldilocksVariable {
+	for i := 0; i < PARTIAL_ROUNDS; i++ {
+		state = Constant(api, rangeChecker, state, *r*SPONGE_WIDTH)
+		state[0] = Sbox(api, rangeChecker, state[0])
+		state = Mds(api, rangeChecker, state)
+		*r += 1
+	}
+	return state
 }
 
 func Permute(api frontend.API, rangeChecker frontend.Rangechecker, inputs []GoldilocksVariable) []GoldilocksVariable {
 	if len(inputs) != SPONGE_WIDTH {
 		panic("Invalid number of inputs")
-	}
-	full_rounds_half := 4
-	partial_rounds := 22
-	constants := POSEIDON_CONSTANTS()
-	if len(constants) != MAX_WIDTH*N_ROUNDS {
-		panic("Incorrect number of constants")
-	}
-	mds_circ := POSEIDON_MDS_CIRC()
-	if len(mds_circ) != SPONGE_WIDTH {
-		panic("Incorrect number of constants")
-	}
-	mds_diag := POSEIDON_MDS_DIAG()
-	if len(mds_diag) != SPONGE_WIDTH {
-		panic("Incorrect number of constants")
 	}
 
 	state := make([]GoldilocksVariable, SPONGE_WIDTH)
@@ -58,30 +72,14 @@ func Permute(api frontend.API, rangeChecker frontend.Rangechecker, inputs []Gold
 	}
 
 	r := 0
-	for i := 0; i < full_rounds_half; i++ {
-		state = Constant(api, rangeChecker, state, constants, r*SPONGE_WIDTH)
-		for j := 0; j < SPONGE_WIDTH; j++ {
-			state[j] = Sbox(api, rangeChecker, state[j])
-		}
-		state = Mds(api, rangeChecker, state, mds_circ, mds_diag)
-		r += 1
+	state = FullRounds(api, rangeChecker, state, &r)
+	state = PartialRounds(api, rangeChecker, state, &r)
+	state = FullRounds(api, rangeChecker, state, &r)
+
+	if r != 2*FULL_ROUNDS_HALF+PARTIAL_ROUNDS {
+		panic("Invalid number of rounds")
 	}
 
-	for i := 0; i < partial_rounds; i++ {
-		state = Constant(api, rangeChecker, state, constants, r*SPONGE_WIDTH)
-		state[0] = Sbox(api, rangeChecker, state[0])
-		state = Mds(api, rangeChecker, state, mds_circ, mds_diag)
-		r += 1
-	}
-
-	for i := 0; i < full_rounds_half; i++ {
-		state = Constant(api, rangeChecker, state, constants, r*SPONGE_WIDTH)
-		for j := 0; j < SPONGE_WIDTH; j++ {
-			state[j] = Sbox(api, rangeChecker, state[j])
-		}
-		state = Mds(api, rangeChecker, state, mds_circ, mds_diag)
-		r += 1
-	}
 	return state
 }
 
