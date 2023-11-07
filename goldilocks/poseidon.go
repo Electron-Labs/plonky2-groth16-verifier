e55b43d96a81baf458e45f954f70fc12bd136a75
ultrainstinct30 <parth@atomlabs.one>
2023-11-07 21:25:54 +0530

    feat: plonk2 poseidon optimisations

--- goldilocks/poseidon.go
+++ goldilocks/poseidon.go
@@ -9,6 +9,11 @@ import (
 var CONSTANTS []*big.Int = POSEIDON_CONSTANTS()
 var MDS_CIRC []*big.Int = POSEIDON_MDS_CIRC()
 var MDS_DIAG []*big.Int = POSEIDON_MDS_DIAG()
+var FAST_PARTIAL_FIRST_ROUND_CONSTANT []*big.Int = POSEIDON_FAST_PARTIAL_FIRST_ROUND_CONSTANT()
+var FAST_PARTIAL_ROUND_CONSTANTS []*big.Int = POSEIDON_FAST_PARTIAL_ROUND_CONSTANTS()
+var FAST_PARTIAL_ROUND_INITIAL_MATRIX [][]*big.Int = POSEIDON_FAST_PARTIAL_ROUND_INITIAL_MATRIX()
+var FAST_PARTIAL_ROUND_W_HATS [][]*big.Int = POSEIDON_FAST_PARTIAL_ROUND_W_HATS()
+var FAST_PARTIAL_ROUND_VS [][]*big.Int = POSEIDON_FAST_PARTIAL_ROUND_VS()
 
 const FULL_ROUNDS_HALF = 4
 const PARTIAL_ROUNDS = 22
@@ -30,11 +35,55 @@ func Sbox(api frontend.API, rangeChecker frontend.Rangechecker, in GoldilocksVar
 func Mds(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable) []GoldilocksVariable {
 	out := make([]GoldilocksVariable, SPONGE_WIDTH)
 	for i := 0; i < SPONGE_WIDTH; i++ {
-		lc := GoldilocksVariable{Limb: 0}
+		lc := frontend.Variable(0)
 		for j := 0; j < SPONGE_WIDTH; j++ {
-			lc = Add(api, rangeChecker, lc, Mul(api, rangeChecker, GoldilocksVariable{Limb: MDS_CIRC[j]}, in[(i+j)%SPONGE_WIDTH]))
+			lc = api.Add(lc, api.Mul(MDS_CIRC[j], in[(i+j)%SPONGE_WIDTH].Limb))
 		}
-		out[i] = Add(api, rangeChecker, lc, Mul(api, rangeChecker, in[i], GoldilocksVariable{Limb: MDS_DIAG[i]}))
+		lc = api.Add(lc, api.Mul(in[i].Limb, MDS_DIAG[i]))
+		out[i] = Reduce(api, rangeChecker, lc, 96)
+	}
+	return out
+}
+
+func PartialFirstConstantLayer(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable) []GoldilocksVariable {
+	for i, v := range in {
+		in[i] = Add(api, rangeChecker, v, GoldilocksVariable{Limb: FAST_PARTIAL_FIRST_ROUND_CONSTANT[i]})
+	}
+	return in
+}
+
+func MdsPartialLayerInit(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable) []GoldilocksVariable {
+	out := make([]GoldilocksVariable, len(in))
+	out[0] = in[0]
+	for i := 1; i < SPONGE_WIDTH; i++ {
+		out[i] = GoldilocksVariable{Limb: 0}
+	}
+	for i := 1; i < SPONGE_WIDTH; i++ {
+		for j := 1; j < SPONGE_WIDTH; j++ {
+			out[j] = Add(api, rangeChecker, out[j], Mul(api, rangeChecker, in[i], GoldilocksVariable{Limb: FAST_PARTIAL_ROUND_INITIAL_MATRIX[i-1][j-1]}))
+		}
+	}
+	return out
+}
+
+func MdsPartialLayerFast(api frontend.API, rangeChecker frontend.Rangechecker, in []GoldilocksVariable, r int) []GoldilocksVariable {
+	out := make([]GoldilocksVariable, len(in))
+	for i := range out {
+		out[i] = GoldilocksVariable{Limb: 0}
+	}
+	d_sum := frontend.Variable(0)
+	for i := 1; i < SPONGE_WIDTH; i++ {
+		si := in[i].Limb
+		d_sum = api.Add(d_sum, api.Mul(si, FAST_PARTIAL_ROUND_W_HATS[r][i-1]))
+	}
+	s0 := in[0].Limb
+	mds0t0 := big.NewInt(0).Add(MDS_CIRC[0], MDS_DIAG[0])
+	d_sum = api.Add(d_sum, api.Mul(s0, mds0t0))
+	d := Reduce(api, rangeChecker, d_sum, 160)
+	out[0] = d
+	for i := 1; i < SPONGE_WIDTH; i++ {
+		tmp := api.Add(in[i].Limb, api.Mul(in[0].Limb, FAST_PARTIAL_ROUND_VS[r][i-1]))
+		out[i] = Reduce(api, rangeChecker, tmp, 128)
 	}
 	return out
 }
@@ -52,12 +101,14 @@ func FullRounds(api frontend.API, rangeChecker frontend.Rangechecker, state []Go
 }
 
 func PartialRounds(api frontend.API, rangeChecker frontend.Rangechecker, state []GoldilocksVariable, r *int) []GoldilocksVariable {
+	state = PartialFirstConstantLayer(api, rangeChecker, state)
+	state = MdsPartialLayerInit(api, rangeChecker, state)
 	for i := 0; i < PARTIAL_ROUNDS; i++ {
-		state = Constant(api, rangeChecker, state, *r*SPONGE_WIDTH)
 		state[0] = Sbox(api, rangeChecker, state[0])
-		state = Mds(api, rangeChecker, state)
-		*r += 1
+		state[0] = Add(api, rangeChecker, state[0], GoldilocksVariable{Limb: FAST_PARTIAL_ROUND_CONSTANTS[i]})
+		state = MdsPartialLayerFast(api, rangeChecker, state, i)
 	}
+	*r += PARTIAL_ROUNDS
 	return state
 }
 
