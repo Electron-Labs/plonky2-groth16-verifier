17591c6a6c5b028120575cdb0e47dbbaa959ca62
utkarsh-21st <66884129+utkarsh-21st@users.noreply.github.com>
2024-01-09 20:09:31 +0000

    refactor

--- verifier/plonk/gates/gate.go
+++ verifier/plonk/gates/gate.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/Electron-Labs/plonky2-groth16-verifier/goldilocks"
-	algebra "github.com/Electron-Labs/plonky2-groth16-verifier/goldilocks/extension"
 	"github.com/Electron-Labs/plonky2-groth16-verifier/verifier/types"
 	"github.com/consensys/gnark/frontend"
 )
@@ -149,12 +148,4 @@ func EvaluateGateConstraints(
 		}
 	}
 	return constraints
-}
-
-func GetLocalExtAlgebra(wires []goldilocks.GoldilocksExtension2Variable, range_ [2]int) [D][D]frontend.Variable {
-	if range_[1]-range_[0] != D {
-		panic("gate::GetLocalExtAlgebra - range must have `D` elements")
-	}
-	twoWires := [D]goldilocks.GoldilocksExtension2Variable{wires[range_[0]], wires[range_[1]-1]}
-	return algebra.GetVariableArray(twoWires)
 }
--- verifier/plonk/gates/vars.go
+++ verifier/plonk/gates/vars.go
@@ -2,7 +2,9 @@ package gates
 
 import (
 	"github.com/Electron-Labs/plonky2-groth16-verifier/goldilocks"
+	algebra "github.com/Electron-Labs/plonky2-groth16-verifier/goldilocks/extension"
 	"github.com/Electron-Labs/plonky2-groth16-verifier/verifier/types"
+	"github.com/consensys/gnark/frontend"
 )
 
 type EvaluationVars struct {
@@ -13,4 +15,12 @@ type EvaluationVars struct {
 
 func (vars *EvaluationVars) RemovePrefix(num_selectors int) {
 	vars.LocalConstants = vars.LocalConstants[num_selectors:]
+}
+
+func GetLocalExtAlgebra(wires []goldilocks.GoldilocksExtension2Variable, range_ [2]int) [D][D]frontend.Variable {
+	if range_[1]-range_[0] != D {
+		panic("gate::GetLocalExtAlgebra - range must have `D` elements")
+	}
+	twoWires := [D]goldilocks.GoldilocksExtension2Variable{wires[range_[0]], wires[range_[1]-1]}
+	return algebra.GetVariableArray(twoWires)
 }
