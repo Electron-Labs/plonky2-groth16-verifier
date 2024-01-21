84a41bc07348b32c39000c6b90d2ed111a423864
utkarsh-21st <66884129+utkarsh-21st@users.noreply.github.com>
2024-01-21 21:38:49 +0000

    move proof testdata

--- build_plonk.sh
+++ build_plonk.sh
@@ -1,10 +1,10 @@
 go build main.go
 go install
-# plonky2-groth16-verifier buildPlonk --common_data ./data/tendermint/common_data.json
+# plonky2-groth16-verifier buildPlonk --common_data ./testdata/tendermint/common_data.json
 
-# plonky2-groth16-verifier provePlonk --plonky2_proof_path ./data/tendermint/proof_with_pis.json --verifier_only_path ./data/tendermint/verifier_only.json --plonky2_public_inputs_path ./data/tendermint/plonky2_pub_inputs.json --gnark_public_inputs_path ./data/tendermint/gnark_pub_inputs.json --proving_key_path ./data/pk.bin --r1cs_path ./data/r1cs.bin
+# plonky2-groth16-verifier provePlonk --plonky2_proof_path ./testdata/tendermint/proof_with_pis.json --verifier_only_path ./testdata/tendermint/verifier_only.json --plonky2_public_inputs_path ./testdata/tendermint/plonky2_pub_inputs.json --gnark_public_inputs_path ./testdata/tendermint/gnark_pub_inputs.json --proving_key_path ./data/pk.bin --r1cs_path ./data/r1cs.bin
 
-# plonky2-groth16-verifier verifyPlonk --plonkProofPath ./data/proofP --vkey_path ./data/vk.bin --gnark_public_inputs_path ./data/tendermint/gnark_pub_inputs.json
+# plonky2-groth16-verifier verifyPlonk --plonkProofPath ./data/proofP --vkey_path ./data/vk.bin --gnark_public_inputs_path ./testdata/tendermint/gnark_pub_inputs.json
 
 # plonky2-groth16-verifier exportSolPlonk --vkey_path ./data/vk.bin
 
